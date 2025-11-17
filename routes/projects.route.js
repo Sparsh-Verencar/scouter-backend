1171de12c337e6f8c37bcfea9261f5370a1daa76
Sparsh Verencar <verencarsparsh@gmail.com>
2025-11-17 12:45:42 +0530

    add projects to db

--- routes/projects.route.js
+++ routes/projects.route.js
@@ -1,10 +1,10 @@
 // routes/auth.js
 import express from 'express';
-import { getFreelancerProjects} from '../controllers/projects.controller.js';
+import { getFreelancerProjects, createFreelancerProjects} from '../controllers/projects.controller.js';
 
 const projectRoutes = express.Router();
 
 projectRoutes.get('/getProjects', getFreelancerProjects);
-//projectRoutes.post('/createProject', freelancerRegister);
+projectRoutes.post('/addProject', createFreelancerProjects);
 
 export default projectRoutes;
